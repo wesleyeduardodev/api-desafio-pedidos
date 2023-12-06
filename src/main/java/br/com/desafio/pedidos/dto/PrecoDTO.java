743e59063dc59755b23d96d4a6ba3cac1caf2b00
wesleyeduardo.dev <wesleyeduardo.dev@gmail.com>
2023-12-06 16:11:17 -0300

    initial comit

---
+++ src/main/java/br/com/desafio/pedidos/dto/PrecoDTO.java
@@ -0,0 +1,20 @@
+package br.com.desafio.pedidos.dto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import java.math.BigDecimal;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@EqualsAndHashCode(of = {"preco", "quantidadeMinima"})
+public class PrecoDTO {
+
+    private BigDecimal preco;
+
+    @JsonProperty(value = "quantidade_minima")
+    private Integer quantidadeMinima;
+}
