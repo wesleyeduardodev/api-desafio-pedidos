15f74e4c62fe3be155874e3063efa90b813f916f
wesleyeduardo.dev <wesleyeduardo.dev@gmail.com>
2024-01-04 13:20:26 -0300

    ajuste de profiles

--- src/main/java/br/com/desafio/pedidos/integracao/FornecedorResource.java
+++ src/main/java/br/com/desafio/pedidos/integracao/FornecedorResource.java
@@ -14,7 +14,7 @@ import java.util.Objects;
 
 @Component
 @Slf4j
-@Profile({"dev"})
+@Profile({"dev","dev-ide"})
 public class FornecedorResource implements FonecedorStrategy {
 
     @Value(value = "${base.url.fornecedor}")
